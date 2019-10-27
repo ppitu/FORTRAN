23effaecbb1b142d5ae1e1370b303f6e0e3ed711
Piotrek <piotr.chmura.1998@o2.pl>
2019-10-27 13:31:36 +0100

    Add Nauka

---
+++ Nauka/arrayToProcedure.f95
@@ -0,0 +1,34 @@
+program arrayToProcedure
+implicit none
+
+	integer, dimension(5) :: myArray
+	integer :: i
+
+	call fillArray(myArray)
+	call printArray(myArray)
+
+end program arrayToProcedure
+
+subroutine fillArray(a)
+implicit none
+
+	integer, dimension(5), intent(out) :: a
+
+	!local variable
+	integer :: i
+	do i = 1,5
+		a(i) = i
+	end do
+
+end subroutine fillArray
+
+subroutine printArray(a)
+
+	integer, dimension(5) :: a
+	integer :: i
+
+	do i = 1,5
+		print*, a(i)
+	end do
+
+end subroutine printArray
