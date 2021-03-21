c62d82ca719b7abfe5eb54228450b24f832ac893
ppitu <piotr.chmura.1998@o2.pl>
2021-03-21 22:26:28 +0100

    Add

---
+++ Programs/prog2.f95
@@ -0,0 +1,5 @@
+PROGRAM p4
+        IMPLICIT NONE 
+        REAL :: a, b
+        INTEGER :: c
+        OPEN(UNIT=66, FILE="data_in", STATUS="OLD", ACTION="READ", 
---
+++ Programs/prog4.f95
@@ -0,0 +1,24 @@
+PROGRAM p4
+        IMPLICIT NONE
+
+        CHARACTER(LEN=10) :: date
+        CHARACTER(LEN=2) :: month
+
+        WRITE(UNIT=*, FMT=*) "enter date (yyyy-mm-dd)"
+        READ(UNIT=*, FMT=*) date
+        month = date(6:7)
+        WRITE(UNIT=*, FMT="(A10)") month
+
+        SELECT CASE(month)
+                CASE("03":"05")
+                        WRITE(UNIT=*, FMT="(A12)") "to wiosna"
+                CASE("06", "07", "08")
+                        WRITE(UNIT=*, FMT="(A12)") "to lato"
+                CASE DEFAULT
+                        WRITE(UNIT=*, FMT="(A25)") "to nie lato ani wiosna"
+        END SELECT
+
+
+
+        STOP
+END PROGRAM p4
